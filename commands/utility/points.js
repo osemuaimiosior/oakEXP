12becb4215505a52c3fab656995019cda0484a8d
Osemu <vincentaimiosior@gmail.com>
2023-12-19 14:24:35 +0100

    update to point bot (If-else statement)

--- commands/utility/points.js
+++ commands/utility/points.js
@@ -6,6 +6,10 @@ module.exports = {
 		.setDescription('Replies with number of points a user has'),
 	 async execute(interaction) { 
 		const key = `${interaction.user.id}`;
-		await interaction.reply(`You currently have ${interaction.client.points.get(key, "points")} points, and are on level ${interaction.client.points.get(key, "level")}!`);
+		if(!interaction.client.points.get(key, "points")){
+			interaction.reply('To start earing some points, send messages by engagaing in conversations with your peers.');
+		} else {
+			await interaction.reply(`You currently have ${interaction.client.points.get(key, "points")} points, and are on level ${interaction.client.points.get(key, "level")}!`);
+		}	
 	},
 };
